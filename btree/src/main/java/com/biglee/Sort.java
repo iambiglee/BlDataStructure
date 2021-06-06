6759d520c4cd21ec72af8d2fa0e5af58039d6c9f
Big_Lee <oscarlee0509@gmail.com>
2021-06-06 13:02:31 +0800

    修正插入排序,并且测试

--- btree/src/main/java/com/biglee/Sort.java
+++ btree/src/main/java/com/biglee/Sort.java
@@ -1,5 +1,7 @@
 package main.java.com.biglee;
 
+import java.util.Arrays;
+
 /**
  * @author ：DELL
  * @date ：Created in 2021/6/4 9:00
@@ -26,18 +28,25 @@ public class Sort {
     /**
      * 插入排序，稳定性排序，通过拿出一个数，和之前所有的数对比，进行的排序，大于就插入
      */
-    public void insertSort(int[] arr) {
+    public static void insertSort(int[] arr) {
         int length=arr.length;
         if(length<=1) return;
         for (int i = 0; i < length; i++) {
             int value=arr[i];
             int j=i-1;
             for (;j>=0;--j){
-                if(arr[j]>value) arr[j+1]=arr[j];
+                if(arr[j]>value)
+                    arr[j+1]=arr[j];
                 else break;
             }
             arr[j+1]=value;
         }
 
+    }
+
+    public static void main(String[] args) {
+        int[] testArr={2,1,3,4,5,6,8,7};
+        insertSort(testArr);
+        System.out.println(Arrays.toString(testArr));
     }
     }
